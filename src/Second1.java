a510eaf89f128cd7f708dc36322e57a244891353
Tara  Aamo <taraaamo@Taras-MacBook-Pro.local>
2023-09-22 09:35:29 +0200

    hej

---
+++ src/Second1.java
@@ -0,0 +1,9 @@
+import com.sun.tools.javac.Main;
+
+public class Second1 {
+    public static void main(String[] args) {
+        Main1 myCar = new Main1();
+        myCar.fullThrottle();
+        myCar.speed(200);
+    }
+}
